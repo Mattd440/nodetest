8d284338d169dd99d1e7b16722c84bb41ebfabf3
Matt <matt@Matts-MacBook-Pro.local>
2018-01-21 21:36:47 -0500

    fix error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 var app = express();
 
 // register director where partials are
@@ -51,5 +51,5 @@ app.get('/about', (req,res)=>{
 });
 
 app.listen(port , () => {
-    console.log(`Server is up on port `${port});
+    console.log(`Server is up on port ${port}`);
 });
